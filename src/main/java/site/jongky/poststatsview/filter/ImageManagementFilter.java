37621c42604750891656382edf5e4ea185027738
60jong <rudwhd515@gmail.com>
2023-02-15 16:07:43 +0900

    feat : response 에 `Cache-Control : no-cache` 적용을 Filter에서 하도록 구현

---
+++ src/main/java/site/jongky/poststatsview/filter/ImageManagementFilter.java
@@ -0,0 +1,24 @@
+package site.jongky.poststatsview.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ImageManagementFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // response control
+        setCacheControl(response);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void setCacheControl(HttpServletResponse response) {
+        response.addHeader("Cache-Control", "no-cache");
+    }
+}
