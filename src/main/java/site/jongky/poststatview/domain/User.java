57d7c7e560884812109dc0bc2f492a015741d868
60jong <rudwhd515@gmail.com>
2023-03-17 15:01:40 +0900

    feat : User 엔티티 저장, 수정 시간 기록하도록 구현

--- src/main/java/site/jongky/poststatview/domain/User.java
+++ src/main/java/site/jongky/poststatview/domain/User.java
@@ -2,22 +2,39 @@ package site.jongky.poststatview.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
+
     @Column(length = 500)
     private String refreshToken;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
     public User(String username, String refreshToken) {
         this.username = username;
+        this.refreshToken = refreshToken;
+    }
+
+    public void changeRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
 }
